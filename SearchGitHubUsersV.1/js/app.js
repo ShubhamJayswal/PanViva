51cfaa18dbbf8c743ea556024cfb9961e5ee7717
Changing the future! <jayswalshubh@gmail.com>
2018-04-27 21:19:13 +1000

    Update app.js

--- SearchGitHubUsersV.1/js/app.js
+++ SearchGitHubUsersV.1/js/app.js
@@ -72,3 +72,13 @@ function searchClick() {
 
 }
  
+searchCtrl.addEventListener("keyup", function(event) {
+  // Cancel the default action, if needed
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("searchBtnId").click();
+  }
+});
+
